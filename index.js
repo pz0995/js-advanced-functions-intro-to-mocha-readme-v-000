78a64e3548e15744b275a57ddf0f9c43ca0b4ee2
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-12-02 09:38:26 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function theTruth() {
+return true;
+}
+
+function sayHey() {
+return "Hey!";
+}
+
+function sayHeyFriend(friendName) {
+return "Hey, " + friendName+"!";
+}
+
+function favoriteIceCream(iceCream) {
+return "I love "+ iceCream;
+}
+
+function shouting(string) {
+var uppercase = string.toUpperCase();
+return uppercase;
+}
+
+function roundDown(x) {
+var x;
+return Math.floor(Math.round(x*100)/100);
+}
