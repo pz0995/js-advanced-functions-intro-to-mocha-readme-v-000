12e984b928e4da8c23f41c3824fc8f2f700f58d4
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-12-02 08:34:43 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,24 @@
+function theTruth() {
+return true;
+}
+
+function sayHey() {
+return "Hey!";
+}
+
+function sayHeyFriend(friendName) {
+return "Hey, " + friendName;
+}
+
+function favoriteIceCream(iceCream) {
+return "I love "+ iceCream;
+}
+
+function shouting() {
+var sentence = sentence.toUppercase;
+}
+
+function roundDown() {
+var x;
+Math.floor(x);
+}
